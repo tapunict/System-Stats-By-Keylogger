0e5ed4bfa8128fe51ab1a281ac7526776e865d28
Antonio Scardace <33966051+ElephanZ@users.noreply.github.com>
2022-05-31 18:14:05 +0200

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
+docker rm -f $(docker ps -a -q)
+docker volume rm $(docker volume ls -q)
+
 docker compose build server-python
 docker compose build logstash
+docker compose build zookeeper
+docker compose build broker
+docker compose build kafka-ui
+docker compose build init-kafka
 docker compose up -d
