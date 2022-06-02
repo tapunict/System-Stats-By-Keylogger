e2f7bedd816c657cb14eaf5bfcab1039e2280936
Antonio Scardace <33966051+ElephanZ@users.noreply.github.com>
2022-06-03 01:33:05 +0200

    Update server.py

--- server-python/server.py
+++ server-python/server.py
@@ -2,24 +2,7 @@ import socket
 import time
 import csv
 
-
-class Server:
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
-
-    def binding(self, n):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.bind((self.host, self.port))
-        self.sock.listen(n)
-
-    def receive(self):
-        (victim, address) = self.sock.accept()
-        data = victim.recv(10240)
-        return data.decode('utf8')
-
-    def close(self):
-        self.sock.close()
+from threading import Thread
 
 
 class CSV:
@@ -32,10 +15,10 @@ class CSV:
             writer.writerow(row)
 
     def writeMetadata(self, uuid, window, timestampBegin, timestampEnd):
-        self.__write__('csv/metadata.csv', [uuid, window, timestampBegin, timestampEnd])
+        self.__write__('metadata.csv', [uuid, window, timestampBegin, timestampEnd])
 
     def writeLog(self, uuid, log):
-        self.__write__('csv/logs.csv', [uuid, log])
+        self.__write__('logs.csv', [uuid, log])
 
 
 class ExtractFeatures:
@@ -62,12 +45,66 @@ class ExtractFeatures:
         return self.log[2][1:-1]
     
 
-output = CSV()  
+class ClientThread:
+    def __init__(self, victim):
+        self.__conn = victim
+
+    def __receive(self):
+        data = self.__conn.recv(10240)
+        return data.decode('utf8')
+
+    def __consolePrint(self, new_log):
+        print('\n')
+        print(new_log, end='', flush=True)
+
+    def __processing(self, new_log):
+        output = CSV()
+        fts = ExtractFeatures(new_log)
+
+        output.writeMetadata(fts.getUUID(), fts.getWindow(), fts.getTimestampBegin(), fts.getTimestampEnd())
+        output.writeLog(fts.getUUID(), fts.getLogText())
+
+        del fts
+        del output
+        self.__consolePrint(new_log)
+    
+    def run(self):
+        try:
+            self.__processing(self.__receive())
+            
+        except Exception as e:
+            print("Error while receiving:", e)
+            self.__victim.close()
+
+
+class Server:
+    def __init__(self, host, port):
+        self.__host = host
+        self.__port = port
+
+    def __createClientThread(self, conn):
+        victim = ClientThread(conn)
+        victim.run()
+
+    def binding(self, n):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((self.__host, self.__port))
+        self.sock.listen(n)
+
+    def receive(self):
+        while True:
+            (victim, address) = self.sock.accept()
+            Thread(target=self.__createClientThread, args=(victim,)).start()
+
+    def close(self):
+        self.sock.close()
+
+
 server = Server("0.0.0.0", 8800)
 
 while True:
     try:
-        server.binding(1)
+        server.binding(16)
 
     except Exception as e:
         print("Error on connecting:", e)
@@ -76,24 +113,5 @@ while True:
 
     break
 
-while True:
-    try:
-        new_log = server.receive()
-        fts = ExtractFeatures(new_log)
-        
-        output.writeMetadata(fts.getUUID(), fts.getWindow(), fts.getTimestampBegin(), fts.getTimestampEnd())
-        output.writeLog(fts.getUUID(), fts.getLogText())
-
-        print('\n')
-        print(new_log, end='', flush=True)
-
-        del fts
-
-    except Exception as e:
-        print("Error while receiving:", e)
-        server.close()
-
-        break
-
-del server
-del output
+server.receive()
+server.close()
