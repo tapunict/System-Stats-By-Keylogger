f3c3c4d6c1c3c5d216342693ecf2d52cdea6eb65
Antonio Scardace <33966051+ElephanZ@users.noreply.github.com>
2022-06-14 19:23:15 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -13,3 +13,6 @@ docker compose build spark-logs
 docker compose build spark-metadata
 
 docker compose up -d
+
+sleep 30s
+curl -X POST localhost:5601/api/saved_objects/_import?createNewCopies=true -H "kbn-xsrf: true" --form file=@kibana/dashboard/system-stats.ndjson
